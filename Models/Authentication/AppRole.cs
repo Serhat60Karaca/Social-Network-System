b664fcbf652bbdb1c12aae27749df185c38a2a1c
Ttan274 <talhatan01@hotmail.com>
2023-06-25 21:48:55 +0300

    Revert "Main Files"

--- Models/Authentication/AppRole.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace TESNS.Models.Authentication
-{
-    public class AppRole : IdentityRole<int>
-    {
-        public DateTime CreateDate { get; set; }
-    }
-}
