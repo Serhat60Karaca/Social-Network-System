8957b3f889eb1ec64a5fdd9f1141003cac6087c2
HasanAliOzen <hasanali.ozen@outlook.com>
2023-06-30 19:36:14 +0300

    .gitignore added file organized

---
+++ TESNS/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using TESNS.Models.Authentication;
+
+namespace TESNS.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<AppUser>> GetAllUsers();
+        Task<AppUser> GetUserById(string id);
+        bool Add(AppUser user);
+        bool Update(AppUser user);
+        bool Delete(AppUser user);
+        bool Save();
+    }
+}
---
+++ TESNS/Services/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace TESNS.Services
+{
+    public interface IPhotoService
+    {
+        Task<ImageUploadResult> AddPhotoAsync(IFormFile file);
+        Task<DeletionResult> DeletePhotoAsync(string publicUrl);
+    }
+}
